b846b75a6511ea4c427cfaf4a109b59a063a6616
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-19 15:20:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-    :patriarch => {},
-    :matriarch => {},
-    :hero => {},
-    :heroine_friends => []
-      
-      }, 
-    :capulet => {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
-  }
+   }
+}
 
   
 end
