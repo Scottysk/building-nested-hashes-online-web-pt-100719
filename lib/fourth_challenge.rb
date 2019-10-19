7742aaa3a81e18b152ef9a0dac2e02ac5173060f
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-19 15:29:52 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,10 +5,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
+        { :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
         {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    }, 
@@ -21,6 +24,11 @@ def fourth_challenge
         :name => "Nurse",
         :age => "44",
         :attitude => "worried"
+          }
+        {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"  
           }
       ]
    }
